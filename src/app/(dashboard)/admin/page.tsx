6f843cdb55065ced6be1107d8dc32dfd3f1f5837
ija54312 <ija54312@gmail.com>
2025-03-31 19:56:53 -0600

    add admin components

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,4 +1,9 @@
 import UserCard from "@/components/UserCard"
+import CountChart from "@/components/CountChart"
+import AttendanceChart from "@/components/AttendanceChart"
+import FinanceChart from "@/components/FinanceChart"
+import EventCalendar from "@/components/EventCalendar"
+import Announcements from "@/components/Announcements"
 
 const AdminPage = () => {
   
@@ -17,22 +22,22 @@ const AdminPage = () => {
         <div className="flex gap-4 flex-col lg:flex-row">
           {/* COUNT CHART */}
           <div className="w-full lg:w-1/3 h-[450px]">
-            {/* <CountChart /> */}
+            <CountChart />
           </div>
           {/* ATTENDANCE CHART */}
           <div className="w-full lg:w-2/3 h-[450px]">
-            {/* <AttendanceChart /> */}
+            <AttendanceChart />
           </div>
         </div>
         {/* BOTTOM CHART */}
         <div className="w-full h-[500px]">
-          {/* <FinanceChart /> */}
+          <FinanceChart />
         </div>
       </div>
       {/* RIGHT */}
       <div className="w-full lg:w-1/3 flex flex-col gap-8">
-        {/* <EventCalendar />
-        <Announcements/> */}
+        <EventCalendar />
+        <Announcements/>
       </div>
     </div>
   )
